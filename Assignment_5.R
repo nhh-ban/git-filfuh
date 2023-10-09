a7129c947d87a7275c03de6481b2fb67938a0740
Filip Fuhrmann <filip.fuhrmann00@gmail.com>
2023-10-09 12:24:47 +0200

    Starting task 3

--- Assignment_5.R
+++ Assignment_5.R
@@ -3,28 +3,30 @@
 
 # Loading packages
 library(tidyverse) 
+library(ggplot2)
 
 
 ### Problem 2 ----
-# Saving data in a new dataframe
-df <- readLines("suites_dw_Table1.txt")
+# Storing the data
+raw_data <- readLines("suites_dw_Table1.txt")
 
-# Saving variable descriptions in a new data frame
-variable_descriptions <- df[1:11] 
+# Saving variable descriptions as a new value
+variable_descriptions <- raw_data[1:11] 
 
 # Saving variable descriptions in a new txt file
 variable_descriptions %>% cat(file = "Variable_descriptions.txt", sep = "\n")
 
 # Removing row 1 to 12, and 14
-df <- df[-c(1:12, 14)]
+raw_data <- raw_data[-c(1:12, 14)]
 
-# Saving dataframe as .csv
-df %>% cat(file = "Galaxies.csv", sep = "\n")
-
-# Reading .csv
-read_csv("Galaxies.csv")
+# Saving data as Galaxies.csv
+raw_data %>% cat(file = "Galaxies.csv", sep = "\n")
 
 
 ### Problem 3 ----
+## Reading .csv and storing it as a df
+galaxies <- read.csv("Galaxies.csv", sep = "|", header = TRUE)
+
+
 
 
